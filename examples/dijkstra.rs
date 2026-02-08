e89bb499d997720dc83dd43e89e2ea2cac0c3683
northward <naut3@DESKTOP-80UA2N1.localdomain>
2026-02-08 22:54:47 +0900

    docs: add Dijkstra and Floyd-Warshall examples solving AOJ problems

---
+++ examples/dijkstra.rs
@@ -0,0 +1,90 @@
+// AOJ GRL_1_A: Single Source Shortest Path
+// URL: https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/1/GRL_1_A
+#![allow(non_snake_case, unused_must_use, unused_imports)]
+use arithmetic_nonmax::NonMax;
+use std::collections::BinaryHeap;
+use std::io::{self, prelude::*};
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct State {
+    cost: NonMax<u32>,
+    position: usize,
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other
+            .cost
+            .cmp(&self.cost)
+            .then_with(|| self.position.cmp(&other.position))
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::read_to_string(io::stdin()).unwrap(), io::stdout());
+    let (mut stdin, mut buffer) = (stdin.split_whitespace(), io::BufWriter::new(stdout.lock()));
+
+    macro_rules! input {
+        ($t: ty) => {
+            stdin.next().unwrap().parse::<$t>().unwrap()
+        };
+    }
+
+    let V = match stdin.next() {
+        Some(v) => v.parse::<usize>().unwrap(),
+        None => return,
+    };
+    let E = input!(usize);
+    let r = input!(usize);
+
+    let mut adj = vec![vec![]; V];
+    for _ in 0..E {
+        let s = input!(usize);
+        let t = input!(usize);
+        let d = input!(u32);
+        adj[s].push((t, d));
+    }
+
+    let mut dists: Vec<Option<NonMax<u32>>> = vec![None; V];
+    let mut pq = BinaryHeap::new();
+
+    let start_dist = NonMax::new(0).unwrap();
+    dists[r] = Some(start_dist);
+    pq.push(State {
+        cost: start_dist,
+        position: r,
+    });
+
+    while let Some(State { cost, position }) = pq.pop() {
+        if let Some(d) = dists[position] {
+            if cost > d {
+                continue;
+            }
+        }
+
+        for &(next_node, edge_cost) in &adj[position] {
+            let next_dist = cost + edge_cost;
+
+            if dists[next_node].is_none() || Some(next_dist) < dists[next_node] {
+                dists[next_node] = Some(next_dist);
+                pq.push(State {
+                    cost: next_dist,
+                    position: next_node,
+                });
+            }
+        }
+    }
+
+    for d in dists {
+        match d {
+            Some(v) => writeln!(buffer, "{}", v),
+            None => writeln!(buffer, "INF"),
+        };
+    }
+}
---
+++ examples/floyd_warshall.rs
@@ -0,0 +1,75 @@
+// AOJ GRL_1_C: All Pairs Shortest Path
+// URL: https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/1/GRL_1_C
+#![allow(non_snake_case, unused_must_use, unused_imports)]
+use arithmetic_nonmax::NonMax;
+use std::io::{self, prelude::*};
+
+fn main() {
+    let (stdin, stdout) = (io::read_to_string(io::stdin()).unwrap(), io::stdout());
+    let (mut stdin, mut buffer) = (stdin.split_whitespace(), io::BufWriter::new(stdout.lock()));
+
+    macro_rules! input {
+        ($t: ty) => {
+            stdin.next().unwrap().parse::<$t>().unwrap()
+        };
+    }
+
+    let V = match stdin.next() {
+        Some(v) => v.parse::<usize>().unwrap(),
+        None => return,
+    };
+    let E = input!(usize);
+
+    let mut dist = vec![vec![None; V]; V];
+    for i in 0..V {
+        dist[i][i] = Some(NonMax::new(0).unwrap());
+    }
+
+    for _ in 0..E {
+        let s = input!(usize);
+        let t = input!(usize);
+        let d = input!(i32);
+        dist[s][t] = Some(NonMax::new(d).unwrap());
+    }
+
+    // Floyd-Warshall
+    for k in 0..V {
+        for i in 0..V {
+            if dist[i][k].is_none() {
+                continue;
+            }
+            for j in 0..V {
+                if dist[k][j].is_none() {
+                    continue;
+                }
+                let new_dist = dist[i][k].unwrap() + i32::from(dist[k][j].unwrap());
+                if dist[i][j].is_none() || Some(new_dist) < dist[i][j] {
+                    dist[i][j] = Some(new_dist);
+                }
+            }
+        }
+    }
+
+    // Negative cycle check
+    for i in 0..V {
+        if let Some(d) = dist[i][i] {
+            if i32::from(d) < 0 {
+                writeln!(buffer, "NEGATIVE CYCLE");
+                return;
+            }
+        }
+    }
+
+    for i in 0..V {
+        for j in 0..V {
+            match dist[i][j] {
+                Some(d) => write!(buffer, "{}", d),
+                None => write!(buffer, "INF"),
+            };
+            if j < V - 1 {
+                write!(buffer, " ");
+            }
+        }
+        writeln!(buffer);
+    }
+}
